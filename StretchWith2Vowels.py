5e59cb699ec18f53ba25647d33f3969b8942435d
Andrew <andrew.r.mungai@student.uts.edu.au>
2024-03-16 18:41:57 +1100

    lololol

---
+++ StretchWith2Vowels.py
@@ -0,0 +1,18 @@
+def read_sentence():
+    
+
+def is_vowel(c):
+    
+
+def count_vowels(segment):
+
+            
+def matches(word):
+  
+
+def count_matches(sentence):
+    lololololo
+  
+
+if __name__ == "__main__":
+    #Write your code here
